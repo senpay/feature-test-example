2dadbea6bc4470d40864fdc5f4a955fbf20b4846
Alexander Pushkarev <alexspush@gmail.com>
2019-08-23 12:25:02 +0300

    All tests written

--- src/test/java/environment/UserApplicationTest.java
+++ src/test/java/environment/UserApplicationTest.java
@@ -17,4 +17,49 @@ public class UserApplicationTest {
         Assert.assertTrue(expectedUserList.contains("Name: name1 login: username1 password password1"));
     }
 
+    @Test
+    public void shouldReturnErrorOnPasswordContainingOnlyDigits() {
+        UserApplication sut = new UserApplication();
+        Map<String, Object> response = sut.addUser("username1", "name1", "1");
+        Assert.assertEquals("Password does not conform rules", response.get("status"));
+        List<String> expectedUserList = (List<String>) response.get("users");
+        Assert.assertFalse(expectedUserList.contains("Name: name1 login: username1 password 1"));
+    }
+
+    @Test
+    public void shouldReturnErrorOnPasswordContainingOnlyLetters() {
+        UserApplication sut = new UserApplication();
+        Map<String, Object> response = sut.addUser("username1", "name1", "a");
+        Assert.assertEquals("Password does not conform rules", response.get("status"));
+        List<String> expectedUserList = (List<String>) response.get("users");
+        Assert.assertFalse(expectedUserList.contains("Name: name1 login: username1 password a"));
+    }
+
+    @Test
+    public void shouldReturnErrorOnEmptyPassword() {
+        UserApplication sut = new UserApplication();
+        Map<String, Object> response = sut.addUser("username1", "name1", "");
+        Assert.assertEquals("Password cannot be empty", response.get("status"));
+        List<String> expectedUserList = (List<String>) response.get("users");
+        Assert.assertFalse(expectedUserList.contains("Name: name1 login: username1 password "));
+    }
+
+    @Test
+    public void shouldReturnErrorOnEmptyName() {
+        UserApplication sut = new UserApplication();
+        Map<String, Object> response = sut.addUser("username1", "", "password");
+        Assert.assertEquals("Full name cannot be empty", response.get("status"));
+        List<String> expectedUserList = (List<String>) response.get("users");
+        Assert.assertFalse(expectedUserList.contains("Name:  login: username1 password password1"));
+    }
+
+    @Test
+    public void shouldReturnErrorOnEmptyUserName() {
+        UserApplication sut = new UserApplication();
+        Map<String, Object> response = sut.addUser("", "name1", "password");
+        Assert.assertEquals("Login cannot be empty", response.get("status"));
+        List<String> expectedUserList = (List<String>) response.get("users");
+        Assert.assertFalse(expectedUserList.contains("Name: name1 login:  password password1"));
+    }
+
 }
